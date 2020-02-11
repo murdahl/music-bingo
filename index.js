b41e54a4a990d6e700411c989077d6ef7b62b347
Ole Magnus Urdahl <murdahl89@gmail.com>
2020-02-11 22:41:07 +0100

    The start of a crappy music-bingo app

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var port = 3000;
+
+var handlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+// ROUTES
+const musicRoute = require("./routes/music-page");
+
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs"
+  })
+);
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/", musicRoute);
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
---
+++ routes/music-page.js
@@ -0,0 +1,33 @@
+// NOT IN USE
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  var artistUrls = [
+    "https://e.snmc.io/i/150/w/ed9c8f3f7ac4a4dbbc0364a4b25fb0e4/5196436",
+    "https://e.snmc.io/i/150/w/1b4108569782341e9531e7df7c41ba85/5349268",
+    "https://e.snmc.io/i/150/w/d0b0ac67afc27e2d6b24ede5abc0e0c1/6951983",
+    "https://e.snmc.io/i/150/w/dacb91f7deb123b38a937a462224ce33/5805553",
+    "https://e.snmc.io/i/150/w/920c3d7a41b6f4f9b2494ec35b3e8b7f/5375436"
+  ];
+
+  res.render("main", { layout: "index", artistUrls });
+});
+
+router.get("/bearer/:bearer", async (req, res, next) => {
+  // Get 25 music names and imgurls
+  var bearer = req.params.bearer;
+  console.log(bearer);
+  // Fetch artisttable
+
+  res.render("main", { layout: "index", artistTable });
+});
+
+router.post("/:bearer", async (req, res, next) => {
+  // Get 25 music names and imgurls
+  var bearer = req.body.bearer;
+  return res.redirect("/" + bearer);
+});
+
+module.exports = router;
