06679aa1ef3980a0e79d97dea91169ecab097431
Ole Magnus Urdahl <murdahl89@gmail.com>
2020-02-20 21:41:25 +0100

    Herokufixes ?

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 5000;
 
-var handlebars = require("express-handlebars");
-var bodyParser = require("body-parser");
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 // ROUTES
-const musicRoute = require("./routes/music-page");
+const musicRoute = require('./routes/music-page');
 
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.engine(
-  "hbs",
-  handlebars({
-    layoutsDir: __dirname + "/views/layouts",
-    extname: "hbs"
-  })
+	'hbs',
+	handlebars({
+		layoutsDir: __dirname + '/views/layouts',
+		extname: 'hbs'
+	})
 );
 
 app.use(express.json());
@@ -23,8 +23,8 @@ app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/", musicRoute);
+app.use('/', musicRoute);
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
