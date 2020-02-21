d787043c5e05ac3b3ac3e8efae30d1c31915f0a2
Ole Magnus Urdahl <murdahl89@gmail.com>
2020-02-21 11:28:46 +0100

    Well im getting there

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', musicRoute);
 
--- routes/music-page.js
+++ routes/music-page.js
@@ -1,30 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var fetchArtists = require('../lib/fetchArtists');
-router.get('/', (req, res) => {
-	var artistUrls = [
-		'https://e.snmc.io/i/150/w/ed9c8f3f7ac4a4dbbc0364a4b25fb0e4/5196436',
-		'https://e.snmc.io/i/150/w/1b4108569782341e9531e7df7c41ba85/5349268',
-		'https://e.snmc.io/i/150/w/d0b0ac67afc27e2d6b24ede5abc0e0c1/6951983',
-		'https://e.snmc.io/i/150/w/dacb91f7deb123b38a937a462224ce33/5805553',
-		'https://e.snmc.io/i/150/w/920c3d7a41b6f4f9b2494ec35b3e8b7f/5375436'
-	];
 
-	res.render('main', { layout: 'index', artistUrls });
-});
-
-router.get('/bearer/:bearer', async (req, res, next) => {
+router.get('/:bearer', async (req, res, next) => {
 	// Get 25 music names and imgurls
 	var bearerToken = req.params.bearer;
-
+	if (!bearerToken) {
+		res.render('main', { layout: 'index' });
+	}
 	// Fetch artisttable
 	var artists = await fetchArtists.fetchArtists(bearerToken);
 
 	//Scramble 25 tables and return
-	var artistTable = fetchArtists.pickNRandomizedList(artists, 5);
+	var artistTables = fetchArtists.pickNRandomizedList(artists, 25);
 
-	console.log(artistTable);
-	res.render('main', { layout: 'index', artistTable, bearerToken });
+	console.log(artistTables);
+	res.render('main', { layout: 'index', artistTables, bearerToken });
 });
 
 router.post('/', async (req, res, next) => {
