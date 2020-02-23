884abfab202dbc4155217dea6a0d54ccd73e7009
Ole Magnus <murdahl89@gmail.com>
2020-02-23 02:12:25 +0100

    We are officially here guys. V1.0  IT WORKS!

--- routes/music-page.js
+++ routes/music-page.js
@@ -1,32 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var spotifyApi = require('../lib/spotify-api');
 var fetchArtists = require('../lib/fetchArtists');
 
-router.get('/:bearer', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	// Get 25 music names and imgurls
-	var bearerToken = req.params.bearer;
-	if (!bearerToken) {
-		res.render('main', { layout: 'index' });
-	}
+
 	// Fetch artisttable
-	var artists = await fetchArtists.fetchArtists(bearerToken);
+	var artists = await spotifyApi.getSporifyArtistList();
 
 	//Scramble 25 tables and return
 	var artistTables = fetchArtists.pickNRandomizedList(artists, 25);
 
-	console.log(artistTables);
-	res.render('main', { layout: 'index', artistTables, bearerToken });
-});
-
-router.post('/', async (req, res, next) => {
-	// Get 25 music names and imgurls
-	var bearer = req.body.bearer;
-	return res.redirect('/bearer/' + bearer);
-});
-
-router.post('/', async (req, res, next) => {
-	// Get 25 music names and imgurls
-	res.render('main', {});
+	res.render('main', { layout: 'index', artistTables });
 });
 
 module.exports = router;
